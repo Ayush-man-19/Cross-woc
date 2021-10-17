3df3fd239b3a5c93c5a64fe11e166bba5c7a42b7
Ayush-man-19 <66672819+Ayush-man-19@users.noreply.github.com>
2021-10-17 15:27:58 +0530

    imported useState from react, created a toggle function that would open up the answers. Created a loop for each question that will drop down the answer on clicking and on clicking again will close the answer

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useState } from 'react';
 import '../styles/FAQ.css';
 
 function FAQ() {
-    return (
-        <div>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
-            <h1>FAQ</h1>
 
+    const [selected, setSelected] = useState(null)
+
+    const toggle = (i) => {
+        if (selected === i) {
+            return setSelected(null)
+        }
+
+        setSelected(i)
+    }
+    const data = [
+        {
+            question:'What is up',
+            answer:'Nothing much',
+            
+        },
+        {
+            question:'What is up',
+            answer:'Nothing much',
+            
+        },
+        {
+            question:'What is up',
+            answer:'Nothing much',
+            
+        },
+        {
+            question:'What is up',
+            answer:'Nothing much',
+            
+        }
+    ]
+
+    return (
+        <div className = 'wrapper'>
+            <div className = 'accordion'>
+                {data.map((item, i) => (
+                    <div className = 'item'>
+                        <div className = 'title' onClick={() => toggle(i)}>
+                            <h2>{item.question}</h2>
+                            <span>{selected === i ? '-' : '+'}</span>    
+                        </div>
+                        <div className ={selected === i ? 'content-show' : 'content'}>{item.answer}</div>
+                    </div>
+                ))}
+                
+            </div>
         </div>
     )
 }
